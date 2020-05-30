19238d0527a1c1aa0fc85d317c81a780f176d5a7
sulaimon23 <tsulaimon21@yahoo.com>
2020-05-30 23:01:20 +0100

    last

--- scrip.js
+++ scrip.js
@@ -1,13 +1,23 @@
 
 let modal = document.getElementById('modal')
-let add = document.getElementById('add').addEventListener('click', e = () => {
+let inside = document.querySelector('.inside')
+
+// let add = document.getElementById('add').addEventListener('click', e = () => {
+//  modal.style.display = "flex"
+// })
+
+// let close = document.getElementById('close').addEventListener('click', e=()=>{
+//  modal.style.display = "none"
+// })
+
+// let cancel = document.getElementById('cancel').addEventListener('click', e=()=>{
+//  modal.style.display = "none"
+// })
+
+let edit = document.getElementById('edit').addEventListener('click', e = () => {
+ inside.style.display = "block";
+})
+
+mod = () => {
  modal.style.display = "flex"
-})
-
-let close = document.getElementById('close').addEventListener('click', e=()=>{
- modal.style.display = "none"
-})
-
-let cancel = document.getElementById('cancel').addEventListener('click', e=()=>{
- modal.style.display = "none"
-})
+}
